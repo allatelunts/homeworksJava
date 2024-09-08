a27a57ecb111cf8719b4f4f0581702071e81a105
allatelunts <alvardtelunts3@gmail.com>
2024-09-08 18:09:12 +0400

    added one and four task changes

--- fourDigitNumber.java
+++ fourDigitNumber.java
@@ -8,6 +8,11 @@ public class fourDigitNumber {
 //1 task
         System.out.print("Enter number: ");
         int number = scanner.nextInt();
+
+        if (number < 1000 || number > 9999) {
+            System.out.println("Error:  must be a four-digit .");
+            System.exit(1);
+        }
         System.out.println("first number is " + number / 1000);
         System.out.println("Second number is " + (number / 100) % 10);
         System.out.println("Third number is " + (number / 10) % 10);
