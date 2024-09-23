77e82ec65381ff1f48948fc7c95e6e0fe50a5160
allatelunts <alvardtelunts3@gmail.com>
2024-09-23 11:04:25 +0400

    Added SumNumbersDiagonals.java

--- Methods/SumNumbersDiagonals.java
+++ Methods/SumNumbersDiagonals.java
@@ -6,26 +6,31 @@ package Methods;
 public class SumNumbersDiagonals {
     public static void main(String[] args) {
         int [][] matrix = {
-                {1, 7, 8, 6},
-                {2, 3, 0, 1},
-                {4, 2, 1, 0},
-                {7, 5, 8, 6}
+                {1, 7, 8, 1,1},
+                {2, 1, 1, 1,1},
+                {4, 1, 1, 0,1},
+                {1, 1, 8, 1,1},
+                {1, 5, 8, 1,1}
         };
 
         int primaryDiagonalSum = 0;
         int secondaryDiagonalSum = 0;
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             primaryDiagonalSum += matrix[i][i];
             secondaryDiagonalSum += matrix[i][matrix[i].length-1 - i];
 
-
         }
+
+
+        int middleIndex = matrix.length / 2;     //subtract the repeated number
+        primaryDiagonalSum -= matrix[middleIndex][middleIndex];
+
 
         int totalSum = primaryDiagonalSum + secondaryDiagonalSum;
 
 
-        System.out.println("The sum of the numbers on the diagonals is: " + totalSum);
+        System.out.println("The sum of the numbers on the diagonals is: " + totalSum );
     }
 }
 
